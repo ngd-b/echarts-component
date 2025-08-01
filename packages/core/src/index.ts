6e913219b69bc09f1454c3d55fef50dfa654a96a
hboot <bobolity@163.com>
2025-08-02 00:28:30 +0800

    feat(core): 优化组件导出并添加新组件
    - 在 index.ts 中导出所有组件，包括新增的 Legend、Tooltip 等
    - 更新 components 数组以包含所有新组件
    - 添加新组件的类型导出

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -8,15 +8,60 @@ import {
   YAxis,
   Title,
   Grid,
+  Legend,
+  Tooltip,
+  Text,
+  AxisLine,
+  AxisTick,
+  MinorSplitLine,
+  MinorTick,
+  SplitArea,
+  SplitLine,
 } from "./components/index";
 import "virtual:uno.css";
 export * from "./hooks/index";
 
 export type * from "./types/index";
 
-export { VueEcharts, Line, Bar, Pie, XAxis, YAxis, Title, Grid };
+export {
+  VueEcharts,
+  Line,
+  Bar,
+  Pie,
+  XAxis,
+  YAxis,
+  Title,
+  Grid,
+  Legend,
+  Tooltip,
+  Text,
+  AxisLine,
+  AxisTick,
+  MinorSplitLine,
+  MinorTick,
+  SplitArea,
+  SplitLine,
+};
 
-const components = [VueEcharts, Line, Bar, Pie, XAxis, YAxis, Title, Grid];
+const components = [
+  VueEcharts,
+  Line,
+  Bar,
+  Pie,
+  XAxis,
+  YAxis,
+  Title,
+  Grid,
+  Legend,
+  Tooltip,
+  Text,
+  AxisLine,
+  AxisTick,
+  MinorSplitLine,
+  MinorTick,
+  SplitArea,
+  SplitLine,
+];
 export default {
   install(app: App) {
     components.forEach((component) => {
