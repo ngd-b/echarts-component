31f0c71ef3c5e6bcd80a649575289f16b839bd00
hboot <bobolity@163.com>
2025-09-02 17:50:27 +0800

    fix(core): 文本组件类型定义缺失字段
    - 在 text 组件中添加 silent 和 valueAnimation 属性
    - 更新类型定义，增加 AxisLabelBaseOption 类型
    - 调整 TextOptions 类型，支持新的文本标签选项

--- packages/core/src/components/common/text/index.vue
+++ packages/core/src/components/common/text/index.vue
@@ -18,6 +18,8 @@ const textContext = useText();
 const props = withDefaults(defineProps<TextOptions & { prop: TextType }>(), {
   prop: "textStyle",
   show: undefined,
+  silent: undefined,
+  valueAnimation: undefined,
   inside: undefined,
   showMinLabel: undefined,
   showMaxLabel: undefined,
@@ -28,6 +30,7 @@ watch(
   () => props,
   () => {
     let propsData = omitBy(props, isUndefined || "prop");
+
     textContext.updateTextStyle(props.prop, { ...options.value, ...propsData });
   },
   { immediate: true, deep: true }
--- packages/core/src/components/common/text/type.ts
+++ packages/core/src/components/common/text/type.ts
@@ -2,12 +2,19 @@ import type {
   TextCommonOption,
   AxisNameTextStyleOption,
   LabelOption,
+  AxisLabelBaseOption,
 } from "echarts/types/dist/shared";
 
-export { TextCommonOption, AxisNameTextStyleOption, LabelOption };
+export {
+  TextCommonOption,
+  AxisNameTextStyleOption,
+  LabelOption,
+  AxisLabelBaseOption,
+};
 
 export type TextOptions = (
   | TextCommonOption
   | AxisNameTextStyleOption
   | LabelOption
+  | AxisLabelBaseOption
 ) & { show?: boolean };
